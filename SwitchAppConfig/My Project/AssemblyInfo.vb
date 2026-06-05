5cf3fc22158c97da3013f90d1eda42e5ce126d39
joliver1972 <oliverjorge@outlook.com>
2026-06-05 18:42:45 +0200

    Add assembly info

---
+++ SwitchAppConfig/My Project/AssemblyInfo.vb
@@ -0,0 +1,16 @@
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("SwitchAppConfig")>
+<Assembly: AssemblyDescription("Aplicación WinForms para cambiar cadenas de conexión de archivos config")>
+<Assembly: AssemblyCompany("joliver1972")>
+<Assembly: AssemblyProduct("SwitchAppConfig")>
+<Assembly: AssemblyCopyright("Copyright © 2026")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+<Assembly: Guid("0f0d17f9-a12c-4fb6-9d6a-9c4a9f4fd210")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
